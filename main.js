64d8fe2c9f6fefdb80dd28f969785c301fd3da6f
Deric Parra <parradeko@gmail.com>
2021-09-25 13:19:26 -0300

    Adcionar scripts

---
+++ main.js
@@ -0,0 +1,19 @@
+function contar() {
+    let inicio = document.querySelector('input#inicial')
+    let fim = document.querySelector('input#final')
+    let passo = document.querySelector('input#passo')
+    let res = document.querySelector('div#res')
+
+    if ( inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0){
+        window.alert('[ERRO] Faltam dados')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for(let c = i; c<= f; c += p) {
+            res.innerHTML += `${c} \u{1F4C8}`
+        }
+    }
+}
